2cd9e59705ffa5cc821deb5896df529e02645bb9
Sam-Blundell <samcalblundell@googlemail.com>
2020-06-20 21:46:36 +0100

    implement assembly program for multiplication via addition

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,54 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//pseudocode:
+//  n=R0;
+//  sum=0;
+//
+//  LOOP:
+//  if (n == 0) {
+//    goto RETURN;
+//  } else {
+//    sum = sum + R1;
+//    n = n-1;
+//    goto LOOP;
+//  }
+//
+//  RETURN:
+//  R2 = sum;
+
+ 
+    @R0     // initialise n to the value of R0
+    D=M
+    @n
+    M=D
+
+    @sum    // initialise sum to 0
+    M=0
+
+    (LOOP)
+    @n
+    D=M
+    @RETURN
+    D;JEQ   // if n is zero, goto END
+
+    @R1
+    D=M
+    @sum
+    M=M+D   // add the value of R1 to sum
+
+    @n
+    M=M-1   // deduct 1 from the value of n
+
+    @LOOP
+    0;JMP   // goto LOOP
+
+    (RETURN)
+    @sum
+    D=M
+    @R2
+    M=D     // store the value of sum in R2
+
+    (END)
+    @END
+    0;JMP
