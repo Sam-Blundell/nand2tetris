55e5d90e4b11d79367e82134639d5f619ecedaab
Sam-Blundell <samcalblundell@googlemail.com>
2020-06-21 00:11:18 +0100

    implement assembly program for blackening screen when any button is pressed

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,84 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// pseudocode
+// LOOP:
+// n = @kbd;
+// if (@KBD != 0) {
+//        BLACK:
+//        if (n == 24576) {
+//            goto LOOP;
+//        } else {
+//            RAM[n] = -1;
+//            n = n + 1;
+//            goto BLACK;
+//        }
+//    } else {
+//        WHITE:
+//        if (n == 24576) {
+//            goto LOOP;
+//        } else {
+//            RAM[n] = 0;
+//            n = n + 1;
+//            goto WHITE;
+//        }
+//    }
+
+    @SCREEN    // set start of Array at first screen register
+    D=A
+    @arr
+    M=D
+
+    @8192   // set last register offset for screen loop
+    D=A
+    @n
+    M=D
+    
+    (MAIN)
+    @i      // set iterator
+    M=0;
+
+    @KBD    // check keyboard and go to either subroutine
+    D=M
+    @BLACK
+    D;JNE
+
+    (WHITE)
+    @i
+    D=M
+    @n
+    D=D-M
+    @MAIN
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    A=D
+    M=0
+
+    @i
+    M=M+1
+    @WHITE
+    0;JMP
+
+    (BLACK)
+    @i
+    D=M
+    @n
+    D=D-M
+    @MAIN
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    A=D
+    M=-1
+
+    @i
+    M=M+1
+    @BLACK
+    0;JMP
